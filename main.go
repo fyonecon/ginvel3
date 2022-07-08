0c4262ff132d85b3c24030ac6ae71857d3b4f48b
fyonecon <fyonecon@gmail.com>
2022-07-08 10:51:01 +0800

    1

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 // @title Ginvel微服务框架
 // @version gv-3.+
-// @description Ginvel是一个Web+GRPC的Golang微服务框架，Http基于Gin，微服务基于gRPC。框架需要「 Go1.17^ + MySQL5.7^ + Redis5^ + CPU核心2^ 」的运行环境。
+// @description Ginvel是一个MVC+RPC的Golang微服务框架，Http基于Gin，DDD服务基于gRPC。框架需要「 Go1.17^ + MySQL5.7^ + Redis5^ + CPU核心2^ 」的运行环境。
 // @contact.email fyonecon@gmail.com
 // @contact.url https://go.ginvel.com
 // @license.name Apache 2.0
